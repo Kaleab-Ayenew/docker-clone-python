397a23525d8d42bc929fb17a8a02a36dc45be4d0
Kal <kaleab.ayenew2013@gmail.com>
2025-08-14 00:30:54 +0300

    pipe

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     args = sys.argv[4:]
     
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    if completed_process.stdout:
+        print(completed_process.stdout.decode("utf-8"),)
+    if completed_process.stderr:
+        print(completed_process.stderr.decode("utf-8"))
 
 
 if __name__ == "__main__":
